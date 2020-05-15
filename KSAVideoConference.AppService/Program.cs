01875afa616ca319895928c771119bd9fa39e5e1
Mohamed Basem Bahman <eng_m.basem@outlook.com>
2020-05-15 16:43:33 +0200

    .

--- KSAVideoConference.AppService/Program.cs
+++ KSAVideoConference.AppService/Program.cs
@@ -13,10 +13,10 @@ namespace KSAVideoConference.AppService
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
-.ConfigureWebHostDefaults(webBuilder =>
-{
-    webBuilder.UseStartup<Startup>();
-});
+                        .ConfigureWebHostDefaults(webBuilder =>
+                        {
+                            webBuilder.UseStartup<Startup>();
+                        });
         }
     }
 }
